cfc06c436c956f9bf2c443e58216eb9b6c587e85
mayur chipade <chipademayur2001@gmail.com>
2023-10-26 15:40:00 +0530

    flask study

---
+++ Day :1/ifcode.py
@@ -0,0 +1,6 @@
+env=input("Enter the cloud platform:")
+
+if env == "aws" or env =="AWS":
+    print("you are in aws environment")
+else:
+    print("you enter wrong name") 
---
+++ Day:2/calculator.py
@@ -0,0 +1,15 @@
+num1=float(input("Enter number First:"))
+num2=float(input("Enter number second:"))
+
+opr=input("Enter operator:")
+
+if opr == "+":
+    output=num1+num2
+
+if opr == "-":
+    output=num1-num2
+
+if opr == "*":
+    output=num1*num2
+
+print("your output is:",output)
